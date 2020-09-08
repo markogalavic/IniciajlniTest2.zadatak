b4c5c6260da2417df14c949a358732307a5f22f6
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-09-08 16:02:23 +0200

    Add files via upload

---
+++ inicijalnoPonavljanjeMarkoGalavić/inicijalnoPonavljanjeMarkoGalavić/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inicijalnoPonavljanjeMarkoGalavić
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float secs=0;
+            float mnt = 0;
+            Console.WriteLine("Unesi broj sekundi");
+            secs = Int32.Parse(Console.ReadLine());
+
+            mnt = secs / 60;
+
+            Console.WriteLine(mnt + "minuta");
+            Console.ReadKey();
+        }
+    }
+}
